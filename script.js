8983de7195fc17275913da40494a48596adcc67f
Mrdarky611 <joseramoscas1012@gmail.com>
2026-07-06 19:30:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ let indice = 0;
 
 const textoPregunta = document.getElementById("pregunta");
 
+// Verifica si el navegador soporta reconocimiento de voz
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (!SpeechRecognition) {
+    alert("Tu navegador no soporta reconocimiento de voz.");
+}
+
+const reconocimiento = new SpeechRecognition();
+
+reconocimiento.lang = "es-MX";
+reconocimiento.continuous = false;
+reconocimiento.interimResults = false;
+
 function hablar() {
 
     const voz = new SpeechSynthesisUtterance(
@@ -18,7 +31,24 @@ function hablar() {
     voz.lang = "es-MX";
 
     speechSynthesis.cancel();
+
+    voz.onend = () => {
+
+        reconocimiento.start();
+
+    };
+
     speechSynthesis.speak(voz);
+
+}
+
+reconocimiento.onresult = function(event){
+
+    const respuesta = event.results[0][0].transcript;
+
+    alert("Escuché: " + respuesta);
+
+    siguiente();
 
 }
 
